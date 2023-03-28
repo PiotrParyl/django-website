ac1ccd08c47cc1ffaa54fb2ff37db27641d41c89
PiotrParyl <piotrekzrydek@gmail.com>
2023-03-28 23:41:48 +0200

    2st page yoo

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""siemanko"""
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    #strona główna.
+    path('',views.index, name='index'),
+    path('stópka',views.index, name='stópka')
+]

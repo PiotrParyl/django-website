c554489d0a401e59d28beb7c3e3ae724909fb923
PiotrParyl <piotrekzrydek@gmail.com>
2023-04-01 00:11:13 +0200

    creating users account

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 # Create your views here.
 
 def index(request):
@@ -23,3 +24,39 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Dodaj nowy temat."""
+    if request.method != 'POST':
+        # Nie przekazano żadnych danych, należy utworzyć pusty formularz.
+        form = TopicForm()
+    else:
+        # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Wyświetlenie pustego formularza.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Dodanie nowego wpisu dla określonego tematu."""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        # Nie przekazano żadnych danych, należy utworzyć pusty formularz.
+        form = EntryForm()
+    else:
+        # Przekazano dane za pomocą żądania POST, należy je przetworzyć.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Wyświetlenie pustego formularza.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
